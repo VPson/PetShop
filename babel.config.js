dfcb4ddfbdcb51c2be89fd26a60c485a5499f117
Vinicius Pinto <viniciuspinto10vsp@gmail.com>
2022-02-05 19:03:32 -0300

    babel

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ]
+  ],
+  plugins: [
+    ['module.resovler', {
+      alias: {
+        '@modules': './src/modules',
+        '@shared': './src/shared'
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+
+}
