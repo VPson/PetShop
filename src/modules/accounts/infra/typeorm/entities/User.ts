8355a943c82a1f63a64014c60f0b41500fe15972
Vinicius Pinto <viniciuspinto10vsp@gmail.com>
2022-02-06 21:31:30 -0300

    add entity user

---
+++ src/modules/accounts/infra/typeorm/entities/User.ts
@@ -0,0 +1,39 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { va as uuidV4 } from 'uuid';
+
+@Entity('users')
+class User {
+	@PrimaryColumn()
+	id: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	password: string;
+
+	@Column()
+	contact: number;
+
+	@Column()
+	isAdmin: boolean;
+
+	@Column()
+	address: string;
+	
+	@CreateDateColumn()
+	createdAt: Date;
+
+
+
+	constructor() {
+		if (!this.id){
+			this.id = uuidV4();
+		}
+	}
+}
+
+export { User };
